8c6f3a94308df4c8fb51789d6c2349d34aa7284f
Zy344350781 <13501989152@163.com>
2019-03-21 17:10:46 +0800

    update

--- day01/base_type.py
+++ day01/base_type.py
@@ -7,9 +7,9 @@
 #
 # def sub(num3,num4):
 #     return num3-num4
-#    #return num2-num1     #return返回值
+#    #return num2-num1                  #return返回值
 #
-# def str_demo():          #str是字符串类型
+# def str_demo():                       #str是字符串类型
 #     str1 = '1'
 #     print(type(str1))
 #
@@ -24,19 +24,19 @@
 #     print(aint, bint)
 #
 # def float_demo():
-#     afloat = 1.584824       #float小数类型
+#     afloat = 1.584824                  #float小数类型
 #     print(afloat)
 #     print(type(afloat))
 #
 # def str_demo1():
 #     a = 'hello '
 #     b = 'world'
-#     print(a+b)                #字符串拼接
-#     print('你好世界,%s%s'%(a,b))    #%s调用变量,%(a,b)调用哪个变量
+#     print(a+b)                        #字符串拼接
+#     print('你好世界,%s%s'%(a,b))      #%s调用变量,%(a,b)调用a,b变量
 #
-# if __name__ == '__main__':       # 使用main方法
-#     int_demo()                 #引用方法
-#     result1 = add(4,8)         #声明sum变量,引用方法并传参,赋值给sum变量
+# if __name__ == '__main__':            # 使用main方法
+#     int_demo()                        #引用方法
+#     result1 = add(4,8)                #声明result变量,引用方法并传参,赋值给sum变量
 #     print(result1)
 #     result2 = sub(6,85)
 #     print(result2)
@@ -46,7 +46,32 @@
 #     str_demo1()
 #     pass
 
-###
+def str_demo():
+    str1 = 'Hello '
+    str2 = 'World!'
+    print('%s%s'%(str1,str2))
+
+def int_demo(num1,num2):
+    print('num1=%s' % num1)
+    print('num2=%s' % num2)
+    print('交换数值')
+    z = num1
+    num1 = num2
+    num2 = z
+    print('num1=%s' % num1)
+    print('num2=%s' % num2)
+
+def float_demo():
+    aF = 1.987748456216546789156
+    bF = 4.1151248788446498
+    return aF + bF
+
+if __name__ == '__main__':
+    int_demo(2,30)
+    str_demo()
+    float = float_demo()
+    print(float)
+
 
 
 
