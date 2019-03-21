1a11a0406dfa1d70180c350481b7707d42e6ece7
unknown <13501989152@163.com>
2019-03-21 15:14:40 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,68 @@
+# def int_demo():  #声明一个方法名,def声明方法
+#     aint = 1     #声明变量并赋值
+#     print(aint)  #输出变量
+#
+# def add(num1,num2):
+#     return num1+num2
+#
+# def sub(num3,num4):
+#     return num3-num4
+#    #return num2-num1     #return返回值
+#
+# def str_demo():          #str是字符串类型
+#     str1 = '1'
+#     print(type(str1))
+#
+# def number():
+#     aint = 10
+#     bint = 20
+#     cint = 0
+#     print(aint, bint)
+#     cint = aint
+#     aint = bint
+#     bint = cint
+#     print(aint, bint)
+#
+# def float_demo():
+#     afloat = 1.584824       #float小数类型
+#     print(afloat)
+#     print(type(afloat))
+#
+# def str_demo1():
+#     a = 'hello '
+#     b = 'world'
+#     print(a+b)                #字符串拼接
+#     print('你好世界,%s%s'%(a,b))    #%s调用变量,%(a,b)调用哪个变量
+#
+# if __name__ == '__main__':       # 使用main方法
+#     int_demo()                 #引用方法
+#     result1 = add(4,8)         #声明sum变量,引用方法并传参,赋值给sum变量
+#     print(result1)
+#     result2 = sub(6,85)
+#     print(result2)
+#     str_demo()
+#     number()
+#     float_demo()
+#     str_demo1()
+#     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
