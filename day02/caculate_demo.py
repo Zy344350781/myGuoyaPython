6e6d4a30866100887503503f55a79160f267a8e6
Zy344350781 <13501989152@163.com>
2019-03-22 17:21:33 +0800

    update

---
+++ day02/caculate_demo.py
@@ -0,0 +1,13 @@
+a = 10
+b = 3
+c = 1
+
+if __name__ == '__main__':
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a%b)
+    print(a>b)
+    print(a%b>c)
+
---
+++ day02/self_method.py
@@ -0,0 +1,33 @@
+
+
+if __name__ == '__main__':
+    # range(10)       #从0开始小于10
+    # range(5,10)     #从5开始小于10
+    # range(2,10,2)   #从2开始小于10且步长2
+    # range(10,2,-1)  #从10开始大于2且步长-1
+    # alist = range(10) #声明一个0-9的数组
+
+
+    # for i in range(1,10):
+    #     for j in range(1,10):
+    #         if i<=j :
+    #             a = i * j
+    #             print('%s * %s = %s' % (i, j, a),end='\t')
+    #             if j == 9:
+    #                 print()
+
+    # for i in range(0,5):
+    #     for j in range(i,5):
+    #         print('*',end='   ')
+    #     print()
+
+    # a = 20
+    # b = 30
+    # if a>b:
+    #     print(a)
+    # else:
+    #     print(b)
+
+
+    pass
+
