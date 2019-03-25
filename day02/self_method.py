6df71253794ef858f8a9404851999a641a6d824d
Zy344350781 <13501989152@163.com>
2019-03-25 10:34:54 +0800

    update

--- day02/self_method.py
+++ day02/self_method.py
@@ -1,5 +1,6 @@
+import random#调用random模块
 
-
+#if __name__ == '__main__':
 
     # range(10)       #从0开始小于10
     # range(5,10)     #从5开始小于10
@@ -47,24 +48,52 @@
     #     blist.append(i)
     # print(blist)
 
-    # 周末作业
-    # 写一个方法,传入两个 int参数, 将两个参数之间的 偶数加起来
-def int():
-    a = 100
-    b = 2
-    sum = 0
-    if a < b:
-        for i in range(a + 1, b):
-            if i % 2 == 0:
-                sum = sum + i
-    elif a > b:
-        for i in range(b + 1, a):
-            if i % 2 == 0:
-                sum += i
-    else:
-        print('两数相等')
-    print(sum)
+#     # 周末作业
+#     # 写一个方法,传入两个 int参数, 将两个参数之间的 偶数加起来
+# def int(a,b):
+#     sum = 0
+#     if a < b:
+#         for i in range(a + 1, b):
+#             if i % 2 == 0:
+#                 sum = sum + i
+#     elif a > b:
+#         for i in range(b + 1, a):
+#             if i % 2 == 0:
+#                 sum += i
+#     else:
+#         print('两数相等')
+#         pass
+#     print('a = %s,b = %s,两者之间偶数相加sum = %s'%(a,b,sum))
+#
+# if __name__ == '__main__':
+#     int(random.randint(0,1000),random.randint(0,1000))#生成一个在0-1000中的随机数 random.randint(0,1000)
+#     pass
 
-if __name__ == '__main__':
-    int()
-    pass
+
+
+#     # 周末作业
+#     # 写一个方法,输入两个 int参数, 将两个参数之间的 偶数加起来
+# def int_demo():
+#     a = input('请输入第一个整数:')#调用input函数,输入string类型
+#     a = int(a)#将输入的string类型强转称int
+#     b = input('请输入第二个整数:')
+#     b = int(b)
+#     sum = 0
+#     if a < b:
+#         a = a + 1
+#         for i in range(a, b):
+#             if i % 2 == 0:
+#                 sum = sum + i
+#     elif a > b:
+#         b = b + 1
+#         for i in range(b, a):
+#             if i % 2 == 0:
+#                 sum += i
+#     else:
+#         print('两数相等')
+#         pass
+#     print('a = %s,b = %s,两者之间偶数相加sum = %s'%(a,b,sum))
+#
+# if __name__ == '__main__':
+#     int_demo()
+#     pass
---
+++ day03/new.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    i = 1
+    while i<6: #while 循环 条件为真,则无限循环(死循环)
+        print(i)
+        i += 1
