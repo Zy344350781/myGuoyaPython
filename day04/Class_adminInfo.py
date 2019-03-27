aa0294c36ba712b83cd69824503df544564596cb
Zy344350781 <13501989152@163.com>
2019-03-27 09:46:01 +0800

    update

--- day04/Class_adminInfo.py
+++ day04/Class_adminInfo.py
@@ -1,12 +1,14 @@
 import requests
 
-from day04.Class_login import Class_login
+from day04.Class_login import Class_login#from类文件名import类名
 
 class Class_adminInfo:
     def test_info(self):
-        a = Class_login()
-        a.login_login()
+        a = Class_login()#login类赋予a
+        a.login_login()#a.方法名,使用方法
         url_adminInfo = 'http://192.168.60.132:8080/admin/info'
-        head_adminInfo = {'Authorization':a.tokenHead+' '+a.token}
+        head_adminInfo = {'Authorization':a.tokenHead+' '+a.token}#使用方法后才能调用login类中的变量
         get_adminInfo = requests.get(url=url_adminInfo, headers=head_adminInfo)
         print(get_adminInfo.text)
+        info_json = get_adminInfo.json()
+        assert 200 == info_json['code']
--- day04/Class_login.py
+++ day04/Class_login.py
@@ -1,14 +1,14 @@
 import requests
 
-class Class_login:
-    def login_login(self):
+class Class_login:#定义类名
+    def login_login(self):#定义普通类方法
         url_login = 'http://192.168.60.132:8080/admin/login'
         json_login = {'username': 'admin', 'password': '123456'}
         post_login = requests.post(url=url_login, json=json_login)
         print(post_login.text)
         json_login_ = post_login.json()
         data_login = json_login_['data']
-        self.tokenHead = data_login['tokenHead']
+        self.tokenHead = data_login['tokenHead']#将要使用的变量+self.表示类.变量,可调用
         self.token = data_login['token']
         assert 'token' in json_login_['data']
 
